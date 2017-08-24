b6b1aea766436ac02b6e56bc7b83ba39c00dd196
disaster <linuxworkgroup@hotmail.com>
2017-08-24 11:32:20 +0000

    Try to migrate to alpine

--- root/tmp/install_node.sh
+++ root/tmp/install_node.sh
@@ -19,7 +19,6 @@ addgroup -g 1000 node \
         linux-headers \
         make \
         python \
-  # gpg keys listed at https://github.com/nodejs/node#release-team
   && for key in \
     9554F04D7259F04124DE6B476D5A82AC7E37093B \
     94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
