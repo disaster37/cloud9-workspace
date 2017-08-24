28a208b9afda9b28022b13e1dbbd24d8af5274f0
disaster <linuxworkgroup@hotmail.com>
2017-08-24 15:19:57 +0000

    Try to migrate to alpine

--- root/tmp/install_docker.sh
+++ root/tmp/install_docker.sh
@@ -45,5 +45,5 @@ docker -v
 
 
 # Install docker compose
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+wget "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
