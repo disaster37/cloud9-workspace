c9a790cdb40059550f56eb2bd16853b348396b9e
disaster <linuxworkgroup@hotmail.com>
2017-08-24 08:21:17 +0000

    Try to migrate to alpine

--- root/tmp/install_golang.sh
+++ root/tmp/install_golang.sh
@@ -3,46 +3,45 @@
 
 apk add --no-cache ca-certificates
 
+set -eux;
+apk add --no-cache --virtual .build-deps \
+	bash \
+	gcc \
+	musl-dev \
+	openssl \
+	go \
+	;
 
-
-set -eux; \
-	apk add --no-cache --virtual .build-deps \
-		bash \
-		gcc \
-		musl-dev \
-		openssl \
-		go \
-	; \
-	export \
+export \
 # set GOROOT_BOOTSTRAP such that we can actually build Go
-		GOROOT_BOOTSTRAP="$(go env GOROOT)" \
+    GOROOT_BOOTSTRAP="$(go env GOROOT)" \
 # ... and set "cross-building" related vars to the installed system's values so that we create a build targeting the proper arch
 # (for example, if our build host is GOARCH=amd64, but our build env/image is GOARCH=386, our build needs GOARCH=386)
-		GOOS="$(go env GOOS)" \
-		GOARCH="$(go env GOARCH)" \
-		GO386="$(go env GO386)" \
-		GOARM="$(go env GOARM)" \
-		GOHOSTOS="$(go env GOHOSTOS)" \
-		GOHOSTARCH="$(go env GOHOSTARCH)" \
-	; \
-	\
-	wget -O go.tgz "https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz"; \
-	echo '5f5dea2447e7dcfdc50fa6b94c512e58bfba5673c039259fd843f68829d99fa6 *go.tgz' | sha256sum -c -; \
-	tar -C /usr/local -xzf go.tgz; \
-	rm go.tgz; \
-	\
-	cd /usr/local/go/src; \
-	for p in /go-alpine-patches/*.patch; do \
-		[ -f "$p" ] || continue; \
-		patch -p2 -i "$p"; \
-	done; \
-	./make.bash; \
-	\
-	rm -rf /go-alpine-patches; \
-	apk del .build-deps; \
-	\
-	export PATH="/usr/local/go/bin:$PATH"; \
-	go version
+	GOOS="$(go env GOOS)" \
+	GOARCH="$(go env GOARCH)" \
+	GO386="$(go env GO386)" \
+	GOARM="$(go env GOARM)" \
+	GOHOSTOS="$(go env GOHOSTOS)" \
+	GOHOSTARCH="$(go env GOHOSTARCH)" \
+    ;
+
+wget -O go.tgz "https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz";
+echo '5f5dea2447e7dcfdc50fa6b94c512e58bfba5673c039259fd843f68829d99fa6 *go.tgz' | sha256sum -c -;
+tar -C /usr/local -xzf go.tgz;
+rm go.tgz;
+
+cd /usr/local/go/src;
+for p in /go-alpine-patches/*.patch; do
+		[ -f "$p" ] || continue;
+		patch -p2 -i "$p";
+done;
+./make.bash;
+
+rm -rf /go-alpine-patches;
+apk del .build-deps;
+
+export PATH="/usr/local/go/bin:$PATH";
+go version
 
 GOPATH=/go
 PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
