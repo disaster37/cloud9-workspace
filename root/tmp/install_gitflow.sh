6b4626c7199c140bff16c3437d217499ca498d68
disaster <linuxworkgroup@hotmail.com>
2017-08-25 08:06:53 +0000

    Try to migrate to alpine

--- root/tmp/install_gitflow.sh
+++ root/tmp/install_gitflow.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-wget https://raw.github.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh
+apk add --no-cache --virtual .fetch-deps \
+	curl \
+    ;
+
+curl -LO https://raw.github.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh
 sh gitflow-installer.sh install stable
 rm gitflow-installer.sh
+
+apk del .fetch-deps;
