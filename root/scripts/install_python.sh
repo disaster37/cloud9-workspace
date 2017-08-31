cabdb63420f9ef9dcd25f7eb78e3013c4db9e8cf
Your Name <you@example.com>
2017-08-31 09:53:07 +0000

    Add make package

--- root/scripts/install_python.sh
+++ root/scripts/install_python.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev  libncursesw5-dev xz-utils tk-dev
+apt-get install -y --no-install-recommends build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev  libncursesw5-dev xz-utils tk-dev
 
 cd /tmp
 wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
@@ -14,6 +14,6 @@ sudo make altinstall
 cd ..
 rm -rf Python-${PYTHON_VERSION}
 
-apt-get autoremove -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev  libncursesw5-dev xz-utils tk-dev &&\
+apt-get autoremove -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev  libncursesw5-dev xz-utils tk-dev &&\
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
