c84e7e7e9dde69754a786f1139865fbb3ab406ca
disaster37 <linuxworkgroup@hotmail.com>
2017-12-18 09:00:39 +0000

    Manager user settings

--- root/scripts/start.sh
+++ root/scripts/start.sh
@@ -5,11 +5,21 @@ if [ "x$GIT_REPO" != "x" ]; then
 	git clone $GIT_REPO
 fi
 
+# Manage git config
 if [ -f "/workspace/.gitconfig" ]; then
 	ln -s /workspace/.gitconfig /home/${SERVICE_USER}/.gitconfig
 else
 	touch /workspace/.gitconfig
 	ln -s /workspace/.gitconfig /home/${SERVICE_USER}/.gitconfig
+fi
+
+# Manage cloud9 user setting
+if [ -f "/workspace/.user.settings" ]; then
+	rm /home/${SERVICE_USER}/.c9/user.settings
+	ln -s /workspace/.user.settings /home/${SERVICE_USER}/.c9/user.settings
+else
+	mv /home/${SERVICE_USER}/.c9/user.settings /workspace/.user.settings
+	ln -s /workspace/.user.settings /home/${SERVICE_USER}/.c9/user.settings
 fi
 
 
