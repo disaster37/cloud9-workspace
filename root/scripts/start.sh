b3a79cbffab8eae954a0f850706980dcd52ef3c1
disaster37 <linuxworkgroup@hotmail.com>
2018-06-29 06:28:13 +0000

    Add some initial files

--- root/scripts/start.sh
+++ root/scripts/start.sh
@@ -5,21 +5,43 @@ if [ "x$GIT_REPO" != "x" ]; then
 	git clone $GIT_REPO
 fi
 
+# Manage internal settings
+if [ ! -d "/workspace/.cloud9" ]; then
+	mkdir /workspace/.cloud9
+fi
+
 # Manage git config
-if [ -f "/workspace/.gitconfig" ]; then
-	ln -s /workspace/.gitconfig /home/${SERVICE_USER}/.gitconfig
+if [ -f "/workspace/.cloud9/.gitconfig" ]; then
+	ln -s /workspace/.cloud9/.gitconfig /home/${SERVICE_USER}/.gitconfig
 else
-	touch /workspace/.gitconfig
-	ln -s /workspace/.gitconfig /home/${SERVICE_USER}/.gitconfig
+	touch /workspace/.cloud9/.gitconfig
+	ln -s /workspace/.cloud9/.gitconfig /home/${SERVICE_USER}/.gitconfig
+fi
+
+# Manage docker config
+if [ -d "/workspace/.cloud9/.docker" ]; then
+	ln -s /workspace/.cloud9/.docker /home/${SERVICE_USER}/.docker
+else
+	mkdir /workspace/.cloud9/.docker
+	ln -s /workspace/.cloud9/.docker /home/${SERVICE_USER}/.docker
+fi
+
+# Manage bashrc
+if [ -f "/workspace/.cloud9/.bashrc" ]; then
+	rm /home/${SERVICE_USER}/.bashrc
+	ln -s /workspace/.cloud9/.bashrc /home/${SERVICE_USER}/.bashrc
+else
+	mv /home/${SERVICE_USER}/.bashrc /workspace/.cloud9/.bashrc
+	ln -s /workspace/.cloud9/.bashrc /home/${SERVICE_USER}/.bashrc
 fi
 
 # Manage cloud9 user setting
-if [ -f "/workspace/.user.settings" ]; then
+if [ -f "/workspace/.cloud9/.user.settings" ]; then
 	rm /home/${SERVICE_USER}/.c9/user.settings
-	ln -s /workspace/.user.settings /home/${SERVICE_USER}/.c9/user.settings
+	ln -s /workspace/.cloud9/.user.settings /home/${SERVICE_USER}/.c9/user.settings
 else
-	mv /home/${SERVICE_USER}/.c9/user.settings /workspace/.user.settings
-	ln -s /workspace/.user.settings /home/${SERVICE_USER}/.c9/user.settings
+	mv /home/${SERVICE_USER}/.c9/user.settings /workspace/.cloud9/.user.settings
+	ln -s /workspace/.cloud9/.user.settings /home/${SERVICE_USER}/.c9/user.settings
 fi
 
 
