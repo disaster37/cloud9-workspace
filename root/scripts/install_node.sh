269d925e59a914c3d0377bf4344802d76bcaa1d1
disaster37 <linuxworkgroup@hotmail.com>
2018-08-29 09:10:09 +0000

    Update debian and cloud9

--- root/scripts/install_node.sh
+++ root/scripts/install_node.sh
@@ -4,6 +4,6 @@ echo "############# INSTALL NODEJS #############"
 
 curl -sL https://deb.nodesource.com/setup_${NODE_BRANCH} | bash -
 apt-get update
-apt-get install -y --no-install-recommends nodejs
+apt-get install -y --no-install-recommends nodejs npm
 apt-get clean
 rm -rf /var/lib/apt/lists/
