3af3590c2d47eb8432020c77e3c570ca6217087c
disaster37 <linuxworkgroup@hotmail.com>
2018-12-31 15:03:42 +0100

    Update install_node.sh

--- root/scripts/install_node.sh
+++ root/scripts/install_node.sh
@@ -4,7 +4,6 @@ echo "############# INSTALL NODEJS #############"
 
 curl -sL https://deb.nodesource.com/setup_${NODE_BRANCH} | bash -
 apt-get update
-apt-get install -y --no-install-recommends nodejs npm
-npm install -g npm
+apt-get install -y --no-install-recommends nodejs
 apt-get clean
 rm -rf /var/lib/apt/lists/
